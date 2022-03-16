16f9d649667eeffbac782712d058ce058fd6d60b
Hugo <24863815+bgpvp@users.noreply.github.com>
2022-03-16 23:03:25 +0100

    🎉 Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# games.hugo.rest
+🎮 A page for my games (currently only one but more coming soon)
---
+++ index.js
@@ -0,0 +1,6 @@
+var typed = new Typed(".title", {
+    strings: ["hugo^5000" ,"games.hugo.rest"],
+    typeSpeed: 150,
+    backSpeed: 200
+});
+  
